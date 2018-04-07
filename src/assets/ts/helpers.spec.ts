0132b293dccf00e34171b150d30879fad09c3a3a
VladimirBabchenko <mgrinevitch@gmail.com>
2018-04-07 19:54:59 +0300

    last-commit

--- src/assets/ts/helpers.spec.ts
+++
@@ -1,8 +0,0 @@
-import "jasmine";
-import {forTest} from "./helpers";
-
-describe("forTest", function() {
-    it("it should return 2", function() {
-      expect(forTest()).toBe(2);
-    });
-  });
