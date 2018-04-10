9cbdd6f4b919024df65a3e9d434569bfeb127d1f
VladimirBabchenko <mgrinevitch@gmail.com>
2018-04-10 23:56:06 +0300

    com

--- src/assets/ts/drag-and-drop.ts
+++ src/assets/ts/drag-and-drop.ts
@@ -1,13 +1,20 @@
 import Squad from "./squad";
 import MilitaryResource from "./military-resource";
-
+// var dragTarget;
+// var parentElem;
 export function drag(event) {
+    document.querySelector(".money-justice").innerHTML
     var target = event.target;
+    // dragTarget=target;
+    // parentElem = dragTarget.parentNode;
     var img = target.closest(".draggable");
     if (img.classList.contains("temporary")) img.classList.remove("temporary");
     if (!img) return;
+    if ((Number(document.querySelector(".money-justice").innerHTML.split(/\s/)[1]) - warriorPrice) < 0) return;
+    if ((Number(document.querySelector(".money-evil").innerHTML.split(/\s/)[1]) - warriorPrice) < 0) return;
     img.classList.add("temporary");
     event.dataTransfer.setData("text", target.className);
+    var warriorPrice = Number(target.closest(".warrior").querySelector(".price").innerHTML.split(/\s/)[1]);
 }
 
 export function allowDrop(event) {
@@ -26,10 +33,15 @@ export function drop(event) {
     if (!data) return;
     var elem = document.querySelector(`.${data}`);
     elem.classList.remove("temporary");
-    li.appendChild(elem);
     var warriorPrice = Number(elem.querySelector(".price").innerHTML.split(/\s/)[1]);
+    if ((Number(document.querySelector(".money-justice").innerHTML.split(/\s/)[1]) - warriorPrice) < 0) return;
+    if ((Number(document.querySelector(".money-evil").innerHTML.split(/\s/)[1]) - warriorPrice) < 0) return;
+    li.appendChild(elem);
+    
+    console.log(warriorPrice);
     elem.getAttribute("data-side") === "justice"?
     document.querySelector(".money-justice").innerHTML = "Money<br> " + (Number(document.querySelector(".money-justice").innerHTML.split(/\s/)[1]) - warriorPrice).toString() :
-    document.querySelector(".money-evil").innerHTML = "Money<br> " +(Number(document.querySelector(".money-evil").innerHTML.split(/\s/)[1]) - warriorPrice).toString()
+    document.querySelector(".money-evil").innerHTML = "Money<br> " +(Number(document.querySelector(".money-evil").innerHTML.split(/\s/)[1]) - warriorPrice).toString();
+    // parentElem.appendChild(dragTarget.cloneNode(true))
 }
 
